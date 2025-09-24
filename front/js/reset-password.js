2f71d0795c6d1a8e2dc1e3ecdbda706a5d03afcb
esore03 <esteban.gomez.reyes@correounivalle.edu.do>
2025-09-23 20:19:28 -0500

    auth export problem

--- front/js/reset-password.js
+++ front/js/reset-password.js
@@ -4,7 +4,7 @@ import { showModal } from "./auth.js";
 const API_URL =
   window.location.hostname.includes("localhost")
     ? "/api"
-    : "https://task-app-back.vercel.app/api";
+    : "https://taskapp-aaph.onrender.com/api";
 
 document.addEventListener("DOMContentLoaded", () => {
   const urlParams = new URLSearchParams(window.location.search);
